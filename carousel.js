a7f1fb0c3639588794733516e6da0c2ebb9c8f09
Chercheuse_du_feu <lesyaderkach.lesya@gmail.com>
2022-07-17 20:10:28 +0400

    corrected

--- carousel.js
+++ carousel.js
@@ -1,5 +1,5 @@
 var dot = document.getElementsByClassName("dot");
-var slides = document.getElementById('slides')
+var slides = document.getElementById('slides');
 
 dot[0].onclick = function () { slides.style.transform = "translateX(0px)" };
 dot[1].onclick = function () { slides.style.transform = "translateX(-800px)" };
